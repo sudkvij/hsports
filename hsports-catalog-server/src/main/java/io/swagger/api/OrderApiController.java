e1fbc37d9f573579fc84d67c769950206d255102
sudkvij <sudkvij@gmail.com>
2020-09-26 20:15:26 +0530

    first commit

---
+++ hsports-catalog-server/src/main/java/io/swagger/api/OrderApiController.java
@@ -0,0 +1,56 @@
+package io.swagger.api;
+
+import io.swagger.model.Order;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-09-25T11:12:06.938Z[GMT]")
+@Controller
+public class OrderApiController implements OrderApi {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderApiController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public OrderApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<Order> orderPost(@ApiParam(value = ""  )  @Valid @RequestBody Order body
+) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<Order>(objectMapper.readValue("{\n  \"quantity\" : 1,\n  \"productId\" : 1234,\n  \"id\" : 1,\n  \"orderDate\" : \"2019-01-10T16:37:30-08:00\",\n  \"customer\" : {\n    \"firstName\" : \"John\",\n    \"lastName\" : \"Smith\",\n    \"phone\" : \"555-555-5555\",\n    \"customerNumber\" : \"d290f1ee\"\n  }\n}", Order.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<Order>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<Order>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
---
+++ hsports-catalog-server/src/main/java/io/swagger/model/Customer.java
@@ -0,0 +1,155 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Customer
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-09-25T11:12:06.938Z[GMT]")
+
+
+public class Customer   {
+  @JsonProperty("customerNumber")
+  private String customerNumber = null;
+
+  @JsonProperty("firstName")
+  private String firstName = null;
+
+  @JsonProperty("lastName")
+  private String lastName = null;
+
+  @JsonProperty("phone")
+  private String phone = null;
+
+  public Customer customerNumber(String customerNumber) {
+    this.customerNumber = customerNumber;
+    return this;
+  }
+
+  /**
+   * Get customerNumber
+   * @return customerNumber
+  **/
+  @ApiModelProperty(example = "d290f1ee", required = true, value = "")
+      @NotNull
+
+    public String getCustomerNumber() {
+    return customerNumber;
+  }
+
+  public void setCustomerNumber(String customerNumber) {
+    this.customerNumber = customerNumber;
+  }
+
+  public Customer firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * Get firstName
+   * @return firstName
+  **/
+  @ApiModelProperty(example = "John", required = true, value = "")
+      @NotNull
+
+    public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public Customer lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * Get lastName
+   * @return lastName
+  **/
+  @ApiModelProperty(example = "Smith", required = true, value = "")
+      @NotNull
+
+    public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Customer phone(String phone) {
+    this.phone = phone;
+    return this;
+  }
+
+  /**
+   * Get phone
+   * @return phone
+  **/
+  @ApiModelProperty(example = "555-555-5555", value = "")
+  
+    public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Customer customer = (Customer) o;
+    return Objects.equals(this.customerNumber, customer.customerNumber) &&
+        Objects.equals(this.firstName, customer.firstName) &&
+        Objects.equals(this.lastName, customer.lastName) &&
+        Objects.equals(this.phone, customer.phone);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(customerNumber, firstName, lastName, phone);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Customer {\n");
+    
+    sb.append("    customerNumber: ").append(toIndentedString(customerNumber)).append("\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("    phone: ").append(toIndentedString(phone)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
